4c54503bf4320e83cd90ce2867f6e70b8fed3d13
olekblok <blokaleksander@gmail.com>
2024-04-08 21:11:35 +0200

    add new attributes

--- src/main/java/com/mycompany/mavenproject2/entities/Prediction.java
+++ src/main/java/com/mycompany/mavenproject2/entities/Prediction.java
@@ -1,11 +1,16 @@
 package com.mycompany.mavenproject2.entities;
+
 import lombok.Data;
 
 public abstract @Data class Prediction {
     private String name;
+    private String description;
+    private double cost;
 
-    public Prediction(String name) {
+    public Prediction(String name, String description, double confidenceLevel, double cost) {
         this.name = name;
+        this.description = description;
+        this.cost = cost;
     }
 
     public abstract void predict();
