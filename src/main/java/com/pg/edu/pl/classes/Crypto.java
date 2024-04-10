8a86c6cd4fcef86e3da24cb915733f9df588b7f4
Adam Sobczuk <s188656@student.pg.edu.pl>
2024-04-10 16:27:59 +0200

    Fixed newlines in constructor

--- src/main/java/com/pg/edu/pl/classes/Crypto.java
+++ src/main/java/com/pg/edu/pl/classes/Crypto.java
@@ -11,8 +11,12 @@ public class Crypto extends EquityHolding{
      * in which cryptocurrency is traded in
      * */
     private String currency;
-    protected Crypto(String currency, String name, String symbol, Double price, Double changesPercentage, Double change, Double dayLow, Double dayHigh, Double yearHigh, Double yearLow, Long marketCap, Double priceAvg50, Double priceAvg200, Long volume, Long avgVolume, Double open, Double previousClose, Long sharesOutstanding, Long timestamp) {
-        super(name, symbol, price, changesPercentage, change, dayLow, dayHigh, yearHigh, yearLow, marketCap, priceAvg50, priceAvg200, volume, avgVolume, open, previousClose, sharesOutstanding, timestamp);
+    protected Crypto(String currency, String name, String symbol, Double price, Double changesPercentage,
+                     Double change, Double dayLow, Double dayHigh, Double yearHigh, Double yearLow,
+                     Long marketCap, Double priceAvg50, Double priceAvg200, Long volume, Long avgVolume,
+                     Double open, Double previousClose, Long sharesOutstanding, Long timestamp) {
+        super(name, symbol, price, changesPercentage, change, dayLow, dayHigh, yearHigh, yearLow, marketCap,
+                priceAvg50, priceAvg200, volume, avgVolume, open, previousClose, sharesOutstanding, timestamp);
         this.currency = currency;
     }
 }
