ace6a0598796ec1d06165ef46eb698b71dcbaedb
Jan Ludwicki <s188899@student.pg.edu.pl>
2024-04-10 17:50:43 +0200

    First part of the class done. Wallet attribute to be added after creating it's class (changes didnt add)

--- src/main/java/com/pg/edu/pl/classes/UserProfile.java
+++ src/main/java/com/pg/edu/pl/classes/UserProfile.java
@@ -1,2 +1,23 @@
-package com.pg.edu.pl.classes;public class UserProfile {
+package com.pg.edu.pl.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Setter
+@Getter
+public class UserProfile extends Account{
+    private Color profileBackground;
+    private String name;
+    private String surname;
+
+    protected UserProfile(Color profileBackground, String name, String surname,
+                          String login, String password, String email) {
+        super(login, password, email);
+        this.profileBackground = profileBackground;
+        this.name = name;
+        this.surname = surname;
+    }
 }
