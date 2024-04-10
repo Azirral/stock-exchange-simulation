8ae63e4c4b5ae50ab248a0ef93dd8f286c048f11
olekblok <blokaleksander@gmail.com>
2024-04-10 16:49:09 +0200

    Create prediction class

--- src/main/java/com/pg/edu/pl/classes/Prediction.java
+++ src/main/java/com/pg/edu/pl/classes/Prediction.java
@@ -7,7 +7,7 @@ public abstract @Data class Prediction {
     private String description;
     private double cost;
 
-    public Prediction(String name, String description, double confidenceLevel, double cost) {
+    public Prediction(String name, String description, double cost) {
         this.name = name;
         this.description = description;
         this.cost = cost;
