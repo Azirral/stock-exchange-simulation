9073ddf7755584387813f084a65606d41809fcfd
Jan Ludwicki <s188899@student.pg.edu.pl>
2024-04-10 22:03:54 +0200

    UserProfile class intial stage is finished. Wallet class is added

--- src/main/java/com/pg/edu/pl/classes/UserProfile.java
+++ src/main/java/com/pg/edu/pl/classes/UserProfile.java
@@ -5,12 +5,19 @@ import lombok.Setter;
 
 import java.awt.*;
 
+
+/**
+ * UserProfile is a class that resembles the user's personal and modifiable information
+ * */
 @Setter
 @Getter
 public class UserProfile extends Account{
+    /** Color of the user's profile picture */
     private Color profileBackground;
     private String name;
     private String surname;
+    /** User's wallet in which user's stocks and credit are kept*/
+    private Wallet wallet;
 
     protected UserProfile(Color profileBackground, String name, String surname,
                           String login, String password, String email) {
