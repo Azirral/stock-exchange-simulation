4214c335c5873ad1d4e64f649802c688e2350b48
olekblok <blokaleksander@gmail.com>
2024-04-10 17:02:24 +0200

    add comments and change constructor to lombok

--- src/main/java/com/pg/edu/pl/classes/Prediction.java
+++ src/main/java/com/pg/edu/pl/classes/Prediction.java
@@ -1,17 +1,39 @@
 package com.pg.edu.pl.classes;
 
-import lombok.Data;
+import lombok.*;
 
-public abstract @Data class Prediction {
+import java.util.Date;
+
+
+/**
+ * Prediction class is an abstract class that holds common variables
+ * for StockPrediction and CryptoPrediction classes that inherit from it.
+ */
+@Getter(AccessLevel.PROTECTED)
+@Setter(AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class Prediction {
+    /**
+     * Name of the method used for prediction
+     */
     private String name;
+    /**
+     * The short description of the method
+     */
     private String description;
-    private double cost;
-
-    public Prediction(String name, String description, double cost) {
-        this.name = name;
-        this.description = description;
-        this.cost = cost;
-    }
+    /**
+     * The predicted future price for stock/crypto
+     */
+    private Double predictedPrice;
+    /**
+     * The accuracy of the method based on
+     * training model
+     */
+    private Double accuracy;
+    /**
+     * The date of the predicted price
+     */
+    private Date log_date;
 
     public abstract void predict();
 }
