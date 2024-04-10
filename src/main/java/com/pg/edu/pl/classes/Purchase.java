700785bfe6371bae3ba3d768fb62e35a8b30505c
Kamil Lesiński <s189501@student.pg.edu.pl>
2024-04-10 19:50:49 +0200

    Purchase class almost done, method waiting for implementation

---
+++ src/main/java/com/pg/edu/pl/classes/Purchase.java
@@ -0,0 +1,20 @@
+package com.pg.edu.pl.classes;
+
+import lombok.*;
+
+/**
+ * Purchase class is a subclass of abstract class Transaction. It is responsible for
+ * updating users wallet after purchase transaction has been made.
+ */
+
+public class Purchase extends Transaction {
+
+    public Purchase(EquityHolding equityHolding, Double amount, Long timestamp, Wallet wallet) {
+        super(equityHolding, amount, timestamp, wallet);
+    }
+
+    @Override
+    public void performTransaction() {
+
+    }
+}
