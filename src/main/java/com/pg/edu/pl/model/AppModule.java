3d25094d1eed8abbd349b4f1e3ed2d657988726a
Jan Ludwicki <s188899@student.pg.edu.pl>
2024-04-17 02:24:29 +0200

    Enumerator with colors that will be used for profile background added. Moreover the class Accounts was added which is responsible for logging in registering the user.

--- src/main/java/com/pg/edu/pl/model/AppModule.java
+++ src/main/java/com/pg/edu/pl/model/AppModule.java
@@ -3,47 +3,28 @@ package com.pg.edu.pl.model;
 import com.pg.edu.pl.model.equityEntities.elements.Quote;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class AppModule {
 
-    public static void login() {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Stock Master - Login");
-
-        System.out.print("Enter username: ");
-        try {
-            String enteredUsername = scanner.nextLine();
-
-            System.out.print("Enter password: ");
-            String enteredPassword = scanner.nextLine();
-
-            if (enteredUsername.equals("janekludwicki") && enteredPassword.equals("qwerty")) {
-                System.out.println("Login successful!");
-                // Call your main menu function or perform further actions after successful login
-            } else {
-                System.out.println("Invalid username or password. Please try again.");
-                // You might want to add a loop here to allow the user to retry or exit gracefully
-            }
-        } catch(NoSuchElementException e){
-            System.out.println("Input not available. Please provide valid input.");
-            // Consume the invalid input to prevent an infinite loop
-            scanner.next();
-        }
-    }
     public static void printMainMenu() {
         System.out.println("Welcome to Stock Master - Your Ultimate Stock Simulation Experience!\n");
         System.out.println("Main Menu:\n");
         System.out.println("1. Logging in");
         System.out.println("2. List stocks");
         System.out.println("3. Show UserProfile");
-        System.out.println("4. Exit");
+        System.out.println("4. Register new Account");
+        System.out.println("5. Exit");
     }
     public static void runApplication() {
-        UserProfile user1 = new UserProfile(Color.red, "Jan", "Ludwicki", "janekludwicki",
+        Accounts accounts = new Accounts(new ArrayList<UserProfile>());
+        UserProfile user1 = new UserProfile(ProfileColor.AZURE.getProfileColor(), "Jan", "Ludwicki", "janekludwicki",
                 "qwerty", "halo@wp.pl");
+        accounts.getUsers().add(user1);
         Quote quote1 = new Quote(-0.17, -6.29,
                 "2024-05-09T20:00:00.000+0000", "AAACX",
                 "American Beacon Balanced Fund R5 Class", 6.22, 0.0,
@@ -65,7 +46,7 @@ public class AppModule {
                 int choice = Integer.parseInt(input.trim());
                 switch (choice) {
                     case 1:
-                        login();
+                        accounts.logIn();
                         break;
                     case 2:
                         System.out.println(quote1);
@@ -74,6 +55,9 @@ public class AppModule {
                         System.out.println(user1);
                         break;
                     case 4:
+                        accounts.register();
+                        break;
+                    case 5:
                         System.out.println("Exiting Stock Master. Goodbye!");
                         System.exit(0); // Terminate the program
                         break;
--- src/main/java/com/pg/edu/pl/model/UserProfile.java
+++ src/main/java/com/pg/edu/pl/model/UserProfile.java
@@ -12,7 +12,7 @@ import java.util.Objects;
  * */
 @Setter
 @Getter
-public class UserProfile extends Account implements Cloneable {
+public class UserProfile extends Account implements Cloneable, Comparable<UserProfile> {
     /** Color of the user's profile picture */
     private Color profileBackground;
     private String name;
@@ -28,12 +28,22 @@ public class UserProfile extends Account implements Cloneable {
         this.surname = surname;
     }
 
+    /**
+     * Constructor for users that dont want to choose the background color
+     * */
     protected UserProfile(String name, String surname,
                           String login, String password, String email) {
-        super(login, password, email);
+        super(email, login, password);
         this.profileBackground = null;
         this.name = name;
         this.surname = surname;
+    }
+
+    protected UserProfile (String password, String login) {
+        super(null, login, password);
+        this.profileBackground = null;
+        this.name = null;
+        this.surname = null;
     }
 
     // Override toString method to print the object
@@ -58,5 +68,10 @@ public class UserProfile extends Account implements Cloneable {
         } catch (CloneNotSupportedException e) {
             throw new AssertionError(); // Since UserProfile implements Cloneable, this should never happen
         }
+    }
+
+    @Override
+    public int compareTo(UserProfile o) {
+         return this.getPassword().compareTo(o.getPassword()) + this.getLogin().compareTo(o.getLogin());
     }
 }
