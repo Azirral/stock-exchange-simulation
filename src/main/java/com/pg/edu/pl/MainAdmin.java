977e312f6e8d5d8e56b9dbabf7c3f6899e4572c3
Kamil Lesiński <s189501@student.pg.edu.pl>
2024-04-18 09:10:05 +0200

    finished new entry point to the app

--- src/main/java/com/pg/edu/pl/MainAdmin.java
+++ src/main/java/com/pg/edu/pl/MainAdmin.java
@@ -1,2 +1,10 @@
-package com.pg.edu.pl;public class MainAdmin {
+package com.pg.edu.pl;
+
+import com.pg.edu.pl.model.AppModuleAdmin;
+
+public class MainAdmin {
+    public static void main(String[] args){
+        AppModuleAdmin app = new AppModuleAdmin();
+        app.runApplication();
+    }
 }
