d8fc152ef99f7a31cf83f0d834c306e1cdc4e3bd
Kamil Lesiński <s189501@student.pg.edu.pl>
2024-05-14 22:54:28 +0200

    initial UDP connection

--- src/main/java/com/pg/edu/pl/UdpClient.java
+++ src/main/java/com/pg/edu/pl/UdpClient.java
@@ -1,2 +1,65 @@
-package com.pg.edu.pl;public class UdpClient {
+package com.pg.edu.pl;
+
+// Java program to illustrate Client side
+// Implementation using DatagramSocket
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class UdpClient
+{
+    public static void main(String args[]) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        byte[] receive = new byte[65535];
+
+        // Step 1:Create the socket object for
+        // carrying the data.
+        DatagramSocket ds = new DatagramSocket();
+
+        InetAddress ip = InetAddress.getLocalHost();
+        byte buf[] = null;
+        DatagramPacket dp = null;
+        // loop while user not enters "bye"
+        while (true)
+        {
+            dp = new DatagramPacket(receive, receive.length);
+            ds.receive(dp);
+            String inp = sc.nextLine();
+            System.out.println(data(receive));
+            // convert the String input into the byte array.
+            buf = inp.getBytes();
+
+            // Step 2 : Create the datagramPacket for sending
+            // the data.
+            DatagramPacket DpSend =
+                    new DatagramPacket(buf, buf.length, ip, 1234);
+
+            // Step 3 : invoke the send call to actually send
+            // the data.
+            ds.send(DpSend);
+
+            // break the loop if user enters "bye"
+            if (inp.equals("bye"))
+                break;
+        }
+    }
+
+    public static StringBuilder data(byte[] a)
+    {
+        if (a == null)
+            return null;
+        StringBuilder ret = new StringBuilder();
+        int i = 0;
+        while (a[i] != 0)
+        {
+            ret.append((char) a[i]);
+            i++;
+        }
+        return ret;
+    }
 }
+
+
